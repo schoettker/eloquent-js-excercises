e4bd3bdb9c2aaf08322028ea90a4172f5db91889
schoettker <lennartschoettker@hotmail.com>
2016-10-29 11:48:49 +0200

    add requirement of at least 2 existing plants for a smartplanteater to eat one

--- 07_01_artificial-stupidity.js
+++ 07_01_artificial-stupidity.js
@@ -308,7 +308,8 @@ SmartPlantEater.prototype.act = function(view) {
   if (this.energy > 80 && space)
     return {type: "reproduce", direction: space};
   var plant = view.find("*");
-  if (this.energy <= 30 && plant)
+  var plants = view.findAll("*");
+  if (this.energy <= 30 && plant && plants.length > 1)
     return {type: "eat", direction: plant};
   if (space)
     return {type: "move", direction: space};
