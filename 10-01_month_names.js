ed6812b1e1dca6ed699328cf4afe0702a5fb2571
Schoettker <lennartschoettker@hotmail.com>
2016-11-01 16:35:38 +0100

    add alternative solution which does not work in node but in the browser

--- 10-01_month_names.js
+++ 10-01_month_names.js
@@ -1,4 +1,5 @@
-var month = (function() {
+// Object as Interface
+var month = function() {
   var months = ['January', 'February', 'March', 'April', 'May', 'June','July','August', 'September', 'October','November', 'December'];
 
   return {
@@ -9,7 +10,22 @@ var month = (function() {
       return months.indexOf(name);
     }
   }
-}());
+}();
+
+// ==== ALTERNATIVE ====
+// doesnt work in node but works in the browser
+// (function(exports) {
+//   var months = ['January', 'February', 'March', 'April', 'May', 'June','July','August', 'September', 'October','November', 'December'];
+
+//   exports.name = function(number) {
+//     return months[number];
+//   };
+
+//   exports.number = function(name) {
+//     return months.indexOf(name);
+//   };
+
+// })(this.month = {});
 
 
 console.log(month.name(2));
