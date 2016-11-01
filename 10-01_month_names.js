b8ca8c7a4eb86f695d1826afe766208c061385f7
Schoettker <lennartschoettker@hotmail.com>
2016-11-01 16:20:53 +0100

    add object as function interface for converting numbers to month and vica versa

---
+++ 10-01_month_names.js
@@ -0,0 +1,18 @@
+var month = (function() {
+  var months = ['January', 'February', 'March', 'April', 'May', 'June','July','August', 'September', 'October','November', 'December'];
+
+  return {
+    name: function(number) {
+      return months[number];
+    },
+    number: function(name) {
+      return months.indexOf(name);
+    }
+  }
+}());
+
+
+console.log(month.name(2));
+// → March
+console.log(month.number("November"));
+// // → 10
