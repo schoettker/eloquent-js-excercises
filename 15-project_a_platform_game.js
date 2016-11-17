cdbea7a0b7af942170e72dfb06d229a5b73faebe
schoettker <lennartschoettker@hotmail.com>
2016-11-17 21:51:10 +0100

    fixed trackKeys func, animation now works

--- 15-project_a_platform_game.js
+++ 15-project_a_platform_game.js
@@ -327,18 +327,17 @@ var arrowCodes = {
   38: 'up',
   39: 'right'
 }
-
 function trackKeys(codes) {
   var pressed = Object.create(null);
   function handler(event) {
     if (codes.hasOwnProperty(event.keyCode)) {
-      var down = event.type == 'keydown';
-      pressed[codes[event.keyCode]] == down;
+      var down = event.type == "keydown";
+      pressed[codes[event.keyCode]] = down;
       event.preventDefault();
     }
   }
-  addEventListener('keydown', handler);
-  addEventListener('keyup', handler);
+  addEventListener("keydown", handler);
+  addEventListener("keyup", handler);
   return pressed;
 }
 
