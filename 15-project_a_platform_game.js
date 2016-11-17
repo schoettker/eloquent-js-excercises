ce684ea0de0d32da9363e643e653a54ef40de859
schoettker <lennartschoettker@hotmail.com>
2016-11-17 21:15:25 +0100

    fix typos, no errors in console, does not work anyways atm

--- 15-project_a_platform_game.js
+++ 15-project_a_platform_game.js
@@ -331,13 +331,60 @@ var arrowCodes = {
 function trackKeys(codes) {
   var pressed = Object.create(null);
   function handler(event) {
-    if (code.hasOwnProperty(event.keyCode)) {
+    if (codes.hasOwnProperty(event.keyCode)) {
       var down = event.type == 'keydown';
       pressed[codes[event.keyCode]] == down;
-      event.prevenDefault();
+      event.preventDefault();
     }
   }
-  addEveventListener('keydown', handler);
-  addEveventListener('keyup', handler);
+  addEventListener('keydown', handler);
+  addEventListener('keyup', handler);
   return pressed;
+}
+
+
+// Running The Game
+function runAnimation(frameFunc) {
+  var lastTime = null;
+  function frame(time) {
+    var stop = false;
+    if (lastTime != null) {
+      var timeStep = Math.min(time - lastTime, 100) / 1000;
+      stop = frameFunc(timeStep) === false;
+    }
+    lastTime = time;
+    if (!stop)
+      requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+var arrows = trackKeys(arrowCodes);
+
+function runLevel(level, Display, andThen) {
+  var display = new Display(document.body, level);
+  runAnimation(function(step) {
+    level.animate(step, arrows);
+    display.drawFrame(step);
+    if (level.isFinished()) {
+      display.clear();
+      if (andThen)
+        andThen(level.status);
+      return false;
+    }
+  });
+}
+
+function runGame(plans, Display) {
+  function startLevel(n) {
+    runLevel(new Level(plans[n]), Display, function(status) {
+      if (status == "lost")
+        startLevel(n);
+      else if (n < plans.length - 1)
+        startLevel(n + 1);
+      else
+        console.log("You win!");
+    });
+  }
+  startLevel(0);
 }
